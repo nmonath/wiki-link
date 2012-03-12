95c1d39d847664982c247e311f527e04bfb620ec
Sameer Singh <sameeersingh@gmail.com>
2012-03-12 17:30:32 -0400

    added directory names based on id, alongith commandline args

--- src/main/scala/edu/umass/cs/iesl/wikilink/retrieve/Retrieve.scala
+++ src/main/scala/edu/umass/cs/iesl/wikilink/retrieve/Retrieve.scala
@@ -6,25 +6,36 @@ import org.apache.http.conn.HttpHostConnectException
 import java.util.zip.GZIPOutputStream
 import edu.umass.cs.iesl.wikilink.google._
 import org.apache.http.client.ClientProtocolException
+import cc.refectorie.user.sameer.util.CmdLine
 
+/**
+ * @author brian, sameer
+ */
 object Retrieve {
 
   var baseOutputDir = ""
-  def constructFilePath(i: Int): String = {
-    baseOutputDir + "/" + i.toString
+
+  def constructFilePath(page: Webpage): String = {
+    var i = page.id
+    val firstDir = (i / 1e6).toInt
+    val secondDir = ((i % 1e6) / 1e3).toInt
+    val name = ((i % 1e3)).toInt
+    val dir = "%s/%03d/%03d/".format(baseOutputDir, firstDir, secondDir)
+    new File(dir).mkdirs()
+    "%s%03d".format(dir, name)
   }
 
-  def getOutputStream(file: String, id: Int): OutputStream =
-    new GZIPOutputStream(new FileOutputStream(constructFilePath(id)))
+  def getOutputStream(page: Webpage): OutputStream =
+    new GZIPOutputStream(new FileOutputStream(constructFilePath(page)))
 
-  def writeError(out: OutputStream, e: String): Unit = { out.write(e.getBytes) }
+  def writeError(out: OutputStream, e: String): Unit = {
+    out.write(e.getBytes)
+  }
 
-  def getPage(x: (Webpage, Int), h: Http): Unit = {
-    val (page, i) = x
+  def getPage(page: Webpage, h: Http): Unit = {
+    val out = getOutputStream(page)
 
-    val out = getOutputStream(page.url, i)
-
-    if (page.url.contains("http://") || page.url.contains ("https://")) {
+    if (page.url.contains("http://") || page.url.contains("https://")) {
       try {
 
         h(url(page.url) >>> out)
@@ -38,6 +49,7 @@ object Retrieve {
           }
         }
         case _: IllegalArgumentException => writeError(out, "!!!ILLEGAL_ARGUMENT_EXCEPTION\t" + page.url)
+        case e: Exception => e.printStackTrace(); writeError(out, e.getStackTrace.mkString("\n"))
       }
     }
     else {
@@ -49,22 +61,28 @@ object Retrieve {
   }
 
   def downloadUrls(pages: WebpageIterator): Unit = {
-    pages.zipWithIndex.sliding(100,100).toSeq.par.foreach(chunk => {
+    pages.zipWithIndex.sliding(100, 100).toSeq.par.foreach(chunk => {
       val h = new Http
-      chunk.foreach(getPage(_, h))
+      chunk.foreach(p => getPage(p._1, h))
       h.shutdown()
     })
   }
 
-  def main(args : Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
     // arg0: data file
     // arg1: output directory
     // arg2: number of workers
+    val opts = CmdLine.parse(args)
+    println(opts)
+    val filename = opts.getOrElse("file", "/Users/sameer/tmp/input")
+    val output = opts.getOrElse("output", "/Users/sameer/tmp/output")
+    val takeOnly = opts.getOrElse("take", Int.MaxValue.toString).toInt
+    val workers = opts.getOrElse("workers", "100").toInt
 
-    collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(args(2).toInt)
+    collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(workers)
 
-    baseOutputDir = args(1)
-    val iter = new WebpageIterator(args(0), takeOnly = 10000)
+    baseOutputDir = output
+    val iter = new WebpageIterator(filename, takeOnly)
     downloadUrls(iter)
   }
 
