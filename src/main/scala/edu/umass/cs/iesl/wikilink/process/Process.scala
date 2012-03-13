8d80dde39560c87ff2b3fdf55df53037e75931bc
Brian Martin <martin@cs.umass.edu>
2012-03-13 13:49:35 -0400

    updated Process skeleton

--- src/main/scala/edu/umass/cs/iesl/wikilink/process/Process.scala
+++ src/main/scala/edu/umass/cs/iesl/wikilink/process/Process.scala
@@ -4,23 +4,29 @@ import io.Source
 import java.io._
 import java.util.zip.GZIPInputStream
 import edu.umass.cs.iesl.wikilink.google._
+import edu.umass.cs.iesl.wikilink.retrieve.FilePath
+import cc.refectorie.user.sameer.util.CmdLine
+import xml.XML
+import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 
 object Process {
+  import FilePath._
 
-  var baseOutputDir = ""
-  def constructFilePath(i: Int): String = {
-    baseOutputDir + "/" + i.toString
-  }
-
-  def getSource(file: String, id: Int): Source =
-    Source.fromInputStream(new GZIPInputStream(new FileInputStream(constructFilePath(id))))
+  def getSource(file: String, page: Webpage): Source =
+    Source.fromInputStream(new GZIPInputStream(new FileInputStream(constructFilePath(page))))
 
   def processPage(x: (Webpage, Int)): Unit = {
     val (page, i) = x
 
-    val src = getSource(page.url, i)
+    val src = getSource(page.url, page)
+
+    val parser = XML.withSAXParser((new SAXFactoryImpl).newSAXParser())
 
     try {
+
+      val ns = parser.loadFile(constructFilePath(page))
+      ns \\ "a"
+
 
     } catch {
       case _ => println("oops")
@@ -35,11 +41,15 @@ object Process {
   }
 
   def main(args : Array[String]): Unit = {
-    // arg0: data dir
-    // arg1: output directory
-    // arg2: number of workers
+    val opts = CmdLine.parse(args)
+    println(opts)
+    val input = opts.getOrElse("dir", "/Users/sameer/tmp/input")
+    val output = opts.getOrElse("process-output", "/Users/sameer/tmp/process-output")
+    val takeOnly = opts.getOrElse("take", Int.MaxValue.toString).toInt
+    val workers = opts.getOrElse("workers", "100").toInt
+    val resume = opts.getOrElse("resume", "false").toBoolean
 
-    collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(args(2).toInt)
+    collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(workers)
 
     baseOutputDir = args(1)
     val iter = new WebpageIterator(args(0), takeOnly = 10000)
