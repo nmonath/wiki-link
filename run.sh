0792a9fe55ed5411cd043a452f5fbf23ed2ccd4f
Brian Martin <martin@cs.umass.edu>
2012-03-13 13:48:42 -0400

    run.sh uses sbt i/o mvn

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-mvn scala:run \ 
-  -DmainClass=edu.umass.cs.iesl.wikilink.retrieve.Retrieve \
-  -DaddArgs="@file=/iesl/canvas/sameer/dat/wiki-link/data-00000-of-00001|@output=output|@resume=true|@workers=100"
+sbt "run-main edu.umass.cs.iesl.wikilink.retrieve.Retrieve @file=/iesl/canvas/sameer/dat/wiki-link/data-00000-of-00001 @output=output @resume=true @workers=100"
