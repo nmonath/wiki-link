31cafa03359374ea627aa1c127b2f0f937fd8705
Brian Martin <martin@cs.umass.edu>
2012-04-06 10:24:05 -0400

    filetype distribution script

---
+++ process/ad-hoc/filetype_distribution.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PAGES_DIR=""
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: ./`basename $0` retrieved-pages-dir/"
+  exit 1
+else
+  PAGES_DIR=$1
+fi
+
+find $PAGES_DIR/ -name "*" -type f | parallel -j0 file -b -i > filetypes.dat
+cat filetypes.dat | sort | uniq -c | sort > filetype_distribution.out
