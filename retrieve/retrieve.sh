d1a337f296cdbc4237646261efaadfe63c996a1b
Sameer Singh <sameeersingh@gmail.com>
2013-04-08 23:32:06 -0400

    updated retieval scripts that download the webpages, and split the google data up.

--- retrieve/retrieve.sh
+++ retrieve/retrieve.sh
@@ -18,7 +18,7 @@ then
 	echo "$URLS_FILE already exists... skipping creation"
 else
 	echo "Filtering URL lines to $URLS_FILE..."
-	cat $DATA_FILE | sed '/^URL\t/!d' | sed 's/URL\t//' > $URLS_FILE
+	cat $DATA_FILE | sed '/^URL\t/!d' | sed 's/URL\t//' | nl > $URLS_FILE
 fi
 
 # split the URLs
@@ -29,7 +29,7 @@ else
 	echo "Splitting URLS..."
 	mkdir $SPLIT_DIR
 	cd $SPLIT_DIR
-	split -d -a 5 ../$URLS_FILE 0
+	split --numeric-suffixes --suffix-length=5 --lines=1000 ../$URLS_FILE 0
 	cd ..
 fi
 
@@ -38,7 +38,6 @@ mkdir -p logs
 mkdir -p pages
 
 # run on each chunk of urls
-echo "RUnning 1 chunk at a time..."
+echo "Running 1 chunk at a time..."
 trap "kill 0" SIGINT SIGTERM EXIT
-find ./$SPLIT_DIR/ -name "*" -type f | xargs -n 1 --max-procs=300 ./retrieve_chunk.sh
-
+find ./$SPLIT_DIR/ -name "*" -type f | xargs -n 1 --max-procs=150 ./retrieve_chunk.sh
--- retrieve/retrieve_chunk.sh
+++ retrieve/retrieve_chunk.sh
@@ -11,15 +11,29 @@ mkdir -p $OUTPUT_DIR
 
 if [ ! -e "$LOG_DIR/complete" ]
 then
-  LINE_NUM=0
-  for line in `cat $1`
-  do
-    wget --timeout=20 --tries=2 -o $LOG_DIR/$LINE_NUM -O $OUTPUT_DIR/$LINE_NUM $line
-    gzip $OUTPUT_DIR/$LINE_NUM
-    ((LINE_NUM++))
-  done
+        if [ ! -e "$LOG_DIR/working" ]
+        then
+                #remove potentially, partially downloaded chunk
+                rm -r $LOG_DIR
+                rm -r $OUTPUT_DIR
+                mkdir -p $LOG_DIR
+                mkdir -p $OUTPUT_DIR
 
-  touch $LOG_DIR/complete
+                touch $LOG_DIR/working
+
+                while read line
+                do
+                  ID=`echo $line | awk '{ print $1 }'`
+                  URL=`echo $line | awk '{ print $2 }'`
+                  echo $URL > $LOG_DIR/$ID.url
+                  wget --timeout=20 --tries=2 -o $LOG_DIR/$ID -O $OUTPUT_DIR/$ID $URL
+                done < "$1"
+
+                touch $LOG_DIR/complete
+                rm $LOG_DIR/working
+        else
+                echo "Skipping in-progress chunk:  " `basename $1`
+        fi
 else
-  echo "Skipping complete chunk:  " `basename $1`
+        echo "Skipping complete chunk:  " `basename $1`
 fi
